a8f258ecf305f0d8d4cf8148da38e08bd27c4b1a
Francis Mauny <f_mauny@hotmail.fr>
2022-10-18 16:56:24 +0200

    ajout MaClasseDeOuf

---
+++ src/main/java/com/thales/model/MaClassedeOuf.java
@@ -0,0 +1,11 @@
+package com.thales.model;
+
+public class MaClassedeOuf {
+
+	public final static String MACHAINE="maChaineNeBougeraPas";
+
+	public String getMACHAINE() {
+		return MACHAINE;
+	}
+	
+}
